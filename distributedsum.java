bdb7e85e1ca6959038ec9bd8e6468f0938743e81
rrjj404 <riyaaaa3210@gmail.com>
2025-05-05 07:45:44 +0530

    Update distributedsum.java

--- distributedsum.java
+++ distributedsum.java
@@ -1,45 +1,53 @@
-import mpi.*;
 
-public class distributedsum {
-    public static void main(String args[])throws MPIException{
-        MPI.Init(args);
-        int rank = MPI.COMM_WORLD.Rank();
-        int size = MPI.COMM_WORLD.Size();
+		import mpi.*;
 
-        int[] array = {1,2,3,4,5,6,7,8,9,10};
-        int n = array.length;
-        int local_n = n/size;
-        int remainder = n%size;
-
-        int[] local_array = new [local_n +(rank<remainder? 1:0)];
-
-        int offset = rank*local_n + Math.min(rank,remainder);
-
-        for (int i=0;i<local_array.length;i++){
-            local_array[i] = array[i+offset];
-        }
-        int local_sum = 0;
-        for(int i=o;i<local_array.length;i++){
-            local_sum+=local_array[i];
-        }
-
-        int[] global_array = new int[size];
-        MPI.COMM_WORLD.Allgather(new int[]{local_sum},0,1,MPI.INT,global_array,0,1,MPI.INT);
-
-        if (rank==0){
-            System.out.println("Number of precesses present :"+ size);
-            System.out.println("Intermediate sums: ");
-            int sum = 0;
-            for (int i=0;i<size;i++){
-                sum+=global_array[i];
-                System.out.println("Process"+i+":"+global_array[i]);
-            }
-            System.out.println("Total sum"+ sum);
-
-        }
-        MPI.Finalize();
-
-
-    }
+        public class DistributedSum{
+            public static void main(String args[]) throws MPIException{
+                MPI.Init(args);
+                int rank = MPI.COMM_WORLD.Rank();  // 0 1 2
+                int size= MPI.COMM_WORLD.Size();  //3
+                
+                int[] array= {1,2,3,4,5,6,7,8,9,10};
+                int n= array.length;     //n=10
+                int local_n = n/size;   //3
+                int remainder = n%size;   //1
+                
+                int[] local_array = new int[local_n + (rank< remainder ? 1:0)];
+                // p0     3+1=4
+                //p1     3+0=3
+                //p2     3+0=3
+                
+                
+                int offset= rank*local_n + Math.min(rank, remainder);
+                //p0    0*3+0=0     0 1 2 3
+                //p1    1*3+1=4     4 5 6
+                //p2    2*3+1=7     7 8 9
+                
+                
+        
+		for(int i=0; i<local_array.length; i++){
+			local_array[i]=array[i+offset];
+		}
+		
+		int local_sum=0;
+		for(int i=0; i<local_array.length; i++){
+			local_sum+=local_array[i];
+		}
+		
+		int[] global_array = new int[size];
+		MPI.COMM_WORLD.Allgather(new int[]{local_sum}, 0,1, MPI.INT, global_array, 0, 1, MPI.INT);
+		if(rank==0){
+		System.out.println("Number of Processes present : "+size);
+		int global_sum=0;
+		for(int i=0; i<size; i++){
+			global_sum+=global_array[i];
+			System.out.println("The local sum by process "+i+" : "+global_array[i]);
+		}
+		
+		System.out.println("Final Global Sum : "+global_sum);}
+		MPI.Finalize();
+		
+		
+	}
 
 }
